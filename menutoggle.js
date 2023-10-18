9db614443855fdc95dc813856267684507af6d27
heloisa botelho <100087135+helobotelho@users.noreply.github.com>
2023-10-18 15:19:24 -0300

    tentando arrumar o menu

--- menutoggle.js
+++ menutoggle.js
@@ -1,6 +1,17 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.mobile-menu').classList.toggle('active');
   });
-  
+
+document.addEventListener('DOMContentLoaded', function() {
+    var menuCheckbox = document.getElementById('menuCheckbox');
+
+    window.addEventListener('scroll', function() {
+        if (menuCheckbox.checked) {
+            menuCheckbox.checked = false;
+        }
+    });
+});
+
+
 
   
